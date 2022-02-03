d049ca04cd980e605003fb7d070bed87ba5b7b23
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-03 12:26:13 +0100

    feature/add-mfe1

---
+++ apps/mfe1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-federation-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mfe1';
+}
